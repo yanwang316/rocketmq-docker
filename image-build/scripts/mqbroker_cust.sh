211818e90da610da62128533de5b3082fb35f832
yanwang <yanwang@5i5j.com>
2022-01-29 17:50:47 +0800

    脚本微调

--- image-build/scripts/mqbroker_cust.sh
+++ image-build/scripts/mqbroker_cust.sh
@@ -3,4 +3,4 @@ brokerId=`echo $HOSTNAME | rev | cut -d \- -f 1 | rev`
 sed -i "s#^brokerId = .*#brokerId = $brokerId#g" ${ROCKETMQ_HOME}/conf/broker.conf
 sed -i "s#^brokerName = .*#brokerName = $BROKER_NAME#g" ${ROCKETMQ_HOME}/conf/broker.conf
 sed -i "s#^brokerClusterName = .*#brokerClusterName = $BROKER_CLUSTER_NAME#g" ${ROCKETMQ_HOME}/conf/broker.conf
-sh ./mqbroker -n $EnvNameServiceAddress "$@"
+sh ./mqbroker "$@"
